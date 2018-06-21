ef5a507d1830b0ded061d5f02dc8995f270e6aff
徐家琪 <xujiaqi@promote.cache-dns.local>
2018-06-21 17:27:42 +0800

    更新代码

---
+++ app/config/securecrt.py
@@ -0,0 +1,20 @@
+# -*- encoding:utf-8 -*-
+"""
+@ Created by Seven on  2018/06/20 
+"""
+from datetime import timedelta
+
+# 重要的基本配置
+SECRET_KEY = '7d58afd5-5fdb-48b0-9c99-3466c2838745'
+# JSON_AS_ASCII = False
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:jiandan123%$#@sh-cdb-4r50kts5.sql.tencentcdb.com:63198/example'
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=3600)  # token过期时间
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# mysql 配置
+
+
+# 阿里云邮件服务
+EMAIL_USERNAME = 'jiandan@soo9s.com'
+EMAIL_PASSWORD = 'JIANdan147'
+EMAIL_HOST = 'smtpdm.aliyun.com'
