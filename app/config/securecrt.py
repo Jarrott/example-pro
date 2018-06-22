cc0d66c007b1856227f6ba1e11e0aedeba51c0f6
徐家琪 <xujiaqi@promote.cache-dns.local>
2018-06-22 21:41:57 +0800

    修改配置文件

--- app/config/securecrt.py
+++ app/config/securecrt.py
@@ -4,13 +4,14 @@
 """
 # 重要的基本配置
 SECRET_KEY = '7d58afd5-5fdb-48b0-9c99-3466c2838745'
-# JSON_AS_ASCII = False
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:{}@sh-cdb-4r50kts5.sql.tencentcdb.com:63198/example'.format(
-    'jiandan123*#')
+
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 # mysql 配置
 
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://{name}:{password}@sh-cdb-4r50kts5.sql.tencentcdb.com:63198/{db}' \
+    .format(
+    name='root', password='jiandan123*#', db='example')
 
 # 阿里云邮件服务
 EMAIL_USERNAME = 'jiandan@soo9s.com'
